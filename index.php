0801a8f9938762d33f1c0b2d3a927f6bfa111a5d
Fleklica <andrej2312penezic@gmail.com>
2016-03-08 17:33:19 +0100

    Procistili smo PHP.

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Naslov</title>
+    <meta charset="UTF-8">
+    <title>Forme</title>
 </head>
-
 <body>
-<h1>Prvi</h1>
+<h1>Forme</h1>
 
-<p>
-    <?php
-    echo('Pozdrav iz PHP-a');
-    echo('<br>');
-    echo(date('d.m.Y'));
-    ?>
-</p>
-<p>
-    <?php
-    $dana_do_praznika = 45;
-    echo('Do praznika je ostalo jos: ' . $dana_do_praznika);
-    ?>
-</p>
+
+
 </body>
 </html>
